b6a076e858599723e774efbfdd3579fd6ad92c0f
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-02-10 21:22:39 -0600

    no message

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// array that holds friend data 
+
+var friendList = [
+    {
+        "name":"Giovanni Pardo",
+        "profilePic":"https://drive.google.com/file/d/15D7a95bSj04ttyZaRrUWsrDCgqH__xHU/view",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+          ]
+      }
+    ]
+
+module.exports = friendList;
